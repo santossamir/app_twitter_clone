b0835d79d732fbbdd63c17b5cb097d439fe3e30b
Samir Santos <santossamir@hotmail.com>
2022-05-17 00:48:36 -0300

    Desenvolvendo a Timeline, incluindo e listando os Tweets.

---
+++ App/Models/Tweet.php
@@ -0,0 +1,50 @@
+<?php
+    namespace App\Models;
+
+    use MF\Model\Model;
+
+    class Tweet extends Model {
+
+        private $id;
+        private $id_usuario;
+        private $tweet;
+        private $data;
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        //salvar
+        public function salvar(){
+            $query_salvar = "insert into tb_tweets(id_usuario, tweet)values(:id_usuario, :tweet)";
+            $stmt = $this->db->prepare($query_salvar);
+            $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+            $stmt->bindValue(':tweet', $this->__get('tweet'));
+            $stmt->execute();
+            return $this;
+        }
+
+        //recuperar
+        public function getAll() {
+            $query_recuperar = "
+                select 
+                    t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+                from 
+                    tb_tweets as t
+                    left join tb_usuarios as u on (t.id_usuario = u.id)
+                where
+                    t.id_usuario = :id_usuario
+                order by
+                    t.data desc";
+            $stmt = $this->db->prepare($query_recuperar);
+            $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+            $stmt->execute();
+
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }
+    }
+?>

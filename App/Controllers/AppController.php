ea2851bea91d71e3a2656b2d2e5ca06004613ffe
Samir Santos <santossamir@hotmail.com>
2022-05-17 22:47:13 -0300

    Implementando métodos para seguir ou deixar de seguir usuários, além de incluir uma subconsulta na query do getAll.

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -68,5 +68,28 @@
 
             $this->render('quemSeguir');
         }
+
+        public function acao() {
+            
+            $this->validaAutenticacao();
+
+            //acao
+            $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+            $id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('id', $_SESSION['id']);
+
+            if($acao == 'seguir') {
+                $usuario->seguirUsuario($id_usuario_seguindo);
+
+            }else if($acao == 'deixar_de_seguir'){
+                $usuario->deixarSeguirUsuario($id_usuario_seguindo);
+            }
+
+            header('Location: /quem_seguir');
+
+        }
     }
+
 ?>
--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -80,11 +80,18 @@
         public function getAll() {
             $query = "
             select
-                id, nome, email
+                u.id, u.nome, u.email, (
+                    select
+                        count(*)
+                    from
+                       tb_usuarios_seguidores as us
+                    where
+                        us.id_usuario = :id_usuario and us.id_usuario_seguindo = u.id
+                ) as seguindo_sn
             from 
-                tb_usuarios
+                tb_usuarios as u
             where
-                nome like :nome and id != :id_usuario
+                u.nome like :nome and u.id != :id_usuario
             ";
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(':nome', '%'.$this->__get('nome').'%');
@@ -92,6 +99,28 @@
             $stmt->execute();
 
             return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+        public function seguirUsuario($id_usuario_seguindo) {
+            $query_seguir = "insert into tb_usuarios_seguidores(id_usuario, id_usuario_seguindo)
+                             value(:id_usuario, :id_usuario_seguindo)";
+            $stmt = $this->db->prepare($query_seguir);
+            $stmt->bindValue(':id_usuario' , $this->__get('id'));
+            $stmt->bindValue(':id_usuario_seguindo' , $id_usuario_seguindo);
+            $stmt->execute();
+
+            return true;
+        }
+
+        public function deixarSeguirUsuario($id_usuario_seguindo) {
+           $query_deixar_de_seguir = "delete from tb_usuarios_seguidores where id_usuario = :id_usuario and
+                                      id_usuario_seguindo = :id_usuario_seguindo";
+            $stmt = $this->db->prepare($query_deixar_de_seguir);
+            $stmt->bindValue(':id_usuario', $this->__get('id'));
+            $stmt->bindValue(':id_usuario_seguindo' , $id_usuario_seguindo);
+            $stmt->execute();
+
+            return true;
         }
     }
 ?>
